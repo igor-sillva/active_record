0ff46c35b7c21169af154d638ff832044e15508f
3kg4kR <igor_sillva@hotmail.com>
2015-03-17 11:11:25 -0400

    Postgres fixies

--- examples/index.js
+++ examples/index.js
@@ -9,29 +9,39 @@ ActiveRecord.configure('default_models_path', '.');
 ActiveRecord.Base.connection.set('env', 'prod');
 ActiveRecord.Base.configure_connection('./database.json');
 /* Establish the connection */
-ActiveRecord.Base.establish_connection();
+ActiveRecord.Base.establish_connection(function (){
+	/* Require the modules */
+	var User  = require('./user.js');
+	var Phone = require('./phone.js');
+	var assert = require('assert');
+
+	/* TEST */
+	User.create({
+		name: 'ooofo',
+		password: 'dsadas'
+	})
+
+	User.all(function (err, users){
+		var first = users[0];
+
+		console.log(first.to_json())
+		first.update_attributes({name: 'Jesus Cristo\\\''})
+		console.log(first.errors.full_messages)
+		first
+		.phones(function (err, phones){
+			// console.log(phones)
+		})
+		// User.all()
+	});
+	User.update(1, {name: 'goo'})
+	User.delete(1)
+	User.destroy(1)
+	User.find(1)
+});
 /* CREATE TABLES */
 require('./create_table').createTable();
 
-/* Require the modules */
-var User  = require('./user.js');
-var Phone = require('./phone.js');
-var assert = require('assert');
 
-/* TEST */
-User.create({
-	name: 'ooofo',
-	password: 'dsadas'
-})
-
-User.all(function (err, users){
-	console.log(users)
-	// User.all()
-});
-User.update(1, {name: 'goo'})
-// User.delete(1)
-// User.destroy(1)
-User.find(1)
 
 /* Close connection with Database */
 setTimeout(function() {
